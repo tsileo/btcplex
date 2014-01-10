a2ee2144a68d48b569968795e2ef90fa6ed8279e
Thomas Sileo <thomas.sileo@gmail.com>
2014-01-10 19:52:47 +0100

    Initial commit

---
+++ addresscheck.go
@@ -0,0 +1,90 @@
+package btcplex
+
+import (
+    "bytes"
+    "crypto/sha256"
+    "errors"
+)
+
+// Taken from http://rosettacode.org/wiki/Bitcoin/address_validation#Go
+ 
+// With at least one other bitcoin RC task, this source is styled more like
+// a package to show how functions of the two tasks might be combined into
+// a single package.  It turns out there's not really that much shared code,
+// just the A25 type and doubleSHA256 method, but it's enough to suggest how
+// the code might be organized.  Types, methods, and functions are capitalized
+// where they might be exported from a package.
+ 
+// A25 is a type for a 25 byte (not base58 encoded) bitcoin address.
+type A25 [25]byte
+ 
+func (a *A25) Version() byte {
+    return a[0]
+}
+ 
+func (a *A25) EmbeddedChecksum() (c [4]byte) {
+    copy(c[:], a[21:])
+    return
+}
+ 
+// DoubleSHA256 computes a double sha256 hash of the first 21 bytes of the
+// address.  This is the one function shared with the other bitcoin RC task.
+// Returned is the full 32 byte sha256 hash.  (The bitcoin checksum will be
+// the first four bytes of the slice.)
+func (a *A25) doubleSHA256() []byte {
+    h := sha256.New()
+    h.Write(a[:21])
+    d := h.Sum([]byte{})
+    h = sha256.New()
+    h.Write(d)
+    return h.Sum(d[:0])
+}
+ 
+// ComputeChecksum returns a four byte checksum computed from the first 21
+// bytes of the address.  The embedded checksum is not updated.
+func (a *A25) ComputeChecksum() (c [4]byte) {
+    copy(c[:], a.doubleSHA256())
+    return
+}/* {{header|Go}} */ 
+ 
+// Tmpl and Set58 are adapted from the C solution.
+// Go has big integers but this techinique seems better.
+var tmpl = []byte("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
+ 
+// Set58 takes a base58 encoded address and decodes it into the receiver.
+// Errors are returned if the argument is not valid base58 or if the decoded
+// value does not fit in the 25 byte address.  The address is not otherwise
+// checked for validity.
+func (a *A25) Set58(s []byte) error {
+    for _, s1 := range s {
+        c := bytes.IndexByte(tmpl, s1)
+        if c < 0 {
+            return errors.New("bad char")
+        }
+        for j := 24; j >= 0; j-- {
+            c += 58 * int(a[j])
+            a[j] = byte(c % 256)
+            c /= 256
+        }
+        if c > 0 {
+            return errors.New("too long")
+        }
+    }
+    return nil
+}
+ 
+// ValidA58 validates a base58 encoded bitcoin address.  An address is valid
+// if it can be decoded into a 25 byte address, the version number is 0,
+// and the checksum validates.  Return value ok will be true for valid
+// addresses.  If ok is false, the address is invalid and the error value
+// may indicate why.
+func ValidA58(a58 []byte) (ok bool, err error) {
+    var a A25
+    if err := a.Set58(a58); err != nil {
+        return false, err
+    }
+    if a.Version() != 0 {
+        return false, errors.New("not version 0")
+    }
+    return a.EmbeddedChecksum() == a.ComputeChecksum(), nil
+}
---
+++ block.go
@@ -0,0 +1,92 @@
+package btcplex
+
+import (
+    "fmt"
+    "errors"
+    "github.com/jmhodges/levigo"
+    "encoding/json"
+    "strconv"
+    "github.com/pmylund/go-cache"
+)
+
+type Block struct {
+    Raw []byte `json:"-"`
+    Hash string `json:"hash"`
+    Height uint `json:"height"`
+    Txs []*Tx `json:"tx,omitempty"`
+    Version uint32 `json:"ver"`
+    MerkleRoot string `json:"mrkl_root"`
+    BlockTime uint32 `json:"time"`
+    Bits uint32 `json:"bits"`
+    Nonce uint32 `json:"nonce"`
+    Size uint32 `json:"size"`
+    TxCnt uint32 `json:"n_tx"`
+    TotalBTC uint64 `json:"total_out"`
+    BlockReward float64 `json:"-"`
+    Parent string `json:"prev_block"`
+    Next string  `json:"next_block"`
+}
+
+
+// Return block reward at the given height
+func GetBlockReward(height uint32) (uint64) {
+        return 50e8 >> (height/210000)
+}
+
+// Fetch a single block by hash
+func GetBlock(db *levigo.DB, blockHash string, fetchTx bool) (block *Block, err error) {
+    ro := levigo.NewReadOptions()
+    defer ro.Close()
+    blockDataRaw, blockErr := db.Get(ro, []byte(fmt.Sprintf("bl!%s", blockHash)))
+    if blockErr != nil {
+        err = errors.New("Block not found")
+        return
+    }
+
+
+    //blockStatus, _ := db.Get(ro, []byte(fmt.Sprintf("bl!%v!main", blockHash)))
+    //blockPrv, _ := db.Get(ro, []byte(fmt.Sprintf("bl!%v!prv", blockHash)))
+    
+    if len(blockDataRaw) == 0 {
+        err = errors.New("Block not found")
+        return
+    }
+
+    block = new(Block)
+    err = json.Unmarshal(blockDataRaw, block)
+    if err != nil {
+        return
+    }
+
+    if fetchTx {
+        
+        blockNxts, _ := GetRange(db, []byte(fmt.Sprintf("bl!%v!nxt!", block.Hash)), []byte(fmt.Sprintf("bl!%v!nxt!\xff", block.Hash)))
+        for _, nxt := range blockNxts {
+            blStatus, _ := db.Get(ro, []byte(fmt.Sprintf("bl!%v!main", nxt.Value)))
+            blStatus2, _ := strconv.Atoi(string(blStatus[:]))
+            if blStatus2 == 1 {
+                block.Next = nxt.Value
+            }
+        }
+        txs_kv, _ := GetRange(db, []byte(fmt.Sprintf("bl!%v!tx!", block.Hash)), []byte(fmt.Sprintf("bl!%v!tx!\xff", block.Hash)))
+        
+        for _, tx_kv := range txs_kv {
+            tx, _ := GetTxFromKv(tx_kv)
+            block.Txs = append(block.Txs, tx)
+        }
+    }
+
+    return
+}
+
+func GetBlockCached(c *cache.Cache, db *levigo.DB, blockHash string, fetchTx bool) (block *Block, err error) {
+    cachekey := fmt.Sprintf("block%v", blockHash)
+    blockcached, found := c.Get(cachekey)
+    if found {
+        return blockcached.(*Block), nil
+    } else {
+        block, err := GetBlock(db, blockHash, fetchTx)
+        c.Set(cachekey, block, 0)
+        return block, err
+    }
+}
