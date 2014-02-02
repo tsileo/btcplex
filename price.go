873a9fbc759138850c99cfdd3eec692403ee4024
Thomas Sileo <thomas.sileo@gmail.com>
2014-02-02 14:40:05 +0100

    Added a BitcoinAverage API wrapper

---
+++ price.go
@@ -0,0 +1,28 @@
+package btcplex
+
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+// Return last USD price from BitcoinAverage API
+func GetLastBitcoinPrice() (price float64, err error) {
+	resp, err := http.Get("https://api.bitcoinaverage.com/ticker/global/USD/last")
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	price, err = strconv.ParseFloat(string(body), 10)
+	return
+}
+
+func main() {
+	p, _ := GetLastBitcoinPrice()
+	fmt.Printf("%v", p)
+}
