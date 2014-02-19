0e998c91ad44a4916da1e369035e6423f521baf0
Thomas Sileo <thomas.sileo@gmail.com>
2014-02-19 17:25:24 +0100

    Added an easy pagination method for AddrData

--- pkg/address.go
+++ pkg/address.go
@@ -28,12 +28,9 @@ func GetAddress(rpool *redis.Pool, address string) (addressdata *AddressData, er
     defer c.Close()
 
     addressdata = new(AddressData)
-    txs1 := []*Tx{}
-    txs := []*Tx{}
 
     zkey := fmt.Sprintf("addr:%v", address)
     txscnt, _ := redis.Int(c.Do("ZCARD", zkey))
-    data, _ := redis.Strings(c.Do("ZREVRANGE", zkey, 0, 50))
 
     addressh := new(AddressHash)
     v, err := redis.Values(c.Do("HGETALL", fmt.Sprintf("addr:%v:h", address)))
@@ -44,38 +41,15 @@ func GetAddress(rpool *redis.Pool, address string) (addressdata *AddressData, er
         panic(err)
     }
 
-    for _, txd := range data {
-        tx, _ := GetTx(rpool, txd)
-        txs1 = append(txs1, tx)
-    }
-
     totalreceived := uint64(addressh.TotalReceived)
     totalsent := uint64(addressh.TotalSent)
     finalbalance := totalreceived - totalreceived
     sentcnt, _ := redis.Int(c.Do("ZCARD", fmt.Sprintf("addr:%v:sent", address)))
     receivedcnt, _ := redis.Int(c.Do("ZCARD", fmt.Sprintf("addr:%v:received", address)))
 
-    for _, ctx := range txs1 {
-        txaddressinfo := new(TxAddressInfo)
-        for _, txi := range ctx.TxIns {
-            if txi.PrevOut.Address == address {
-                txaddressinfo.InTxIn = true
-                txaddressinfo.Value -= int64(txi.PrevOut.Value)
-            }
-        }
-        for _, txo := range ctx.TxOuts {
-            if txo.Addr == address {
-                txaddressinfo.InTxOut = true
-                txaddressinfo.Value += int64(txo.Value)
-            }
-        }
-        ctx.TxAddressInfo = txaddressinfo
-        txs = append(txs, ctx)
-    }
-
     //By(TxBlockTime).Sort(txs)
 
-    addressdata.Txs = txs
+    //addressdata.Txs = txs
     addressdata.FinalBalance = finalbalance
     addressdata.TotalSent = totalsent
     addressdata.TotalReceived = totalreceived
@@ -84,6 +58,49 @@ func GetAddress(rpool *redis.Pool, address string) (addressdata *AddressData, er
     addressdata.SentCnt = uint64(sentcnt)
     addressdata.ReceivedCnt = uint64(receivedcnt)
 
+    return
+}
+
+func (addrData *AddressData) FetchTxs(rpool *redis.Pool, start, stop int) (txs []*Tx, err error) {
+    c := rpool.Get()
+    defer c.Close()
+
+    txs = []*Tx{}
+
+    zkey := fmt.Sprintf("addr:%v", addrData.Address)
+
+    data, err := redis.Strings(c.Do("ZREVRANGE", zkey, start, stop))
+    if err != nil {
+        return
+    }
+    txs1 := []*Tx{}
+
+    for _, txd := range data {
+        tx, txerr := GetTx(rpool, txd)
+        if txerr != nil {
+            err = txerr
+            return
+        }
+        txs1 = append(txs1, tx)
+    }
+
+    for _, ctx := range txs1 {
+        txaddressinfo := new(TxAddressInfo)
+        for _, txi := range ctx.TxIns {
+            if txi.PrevOut.Address == addrData.Address {
+                txaddressinfo.InTxIn = true
+                txaddressinfo.Value -= int64(txi.PrevOut.Value)
+            }
+        }
+        for _, txo := range ctx.TxOuts {
+            if txo.Addr == addrData.Address {
+                txaddressinfo.InTxOut = true
+                txaddressinfo.Value += int64(txo.Value)
+            }
+        }
+        ctx.TxAddressInfo = txaddressinfo
+        txs = append(txs, ctx)
+    }
     return
 }
 
