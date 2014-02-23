582206f6382f02ef861e5f085aac39fe1ad90e95
Thomas Sileo <thomas.sileo@gmail.com>
2014-02-23 21:16:47 +0100

    Moved command into packages

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 # Build btcplex and put the binaries in ./bin
 OLD_GOBIN=$GOBIN
 export GOBIN="`pwd`/bin"
-go install -a ./btcplex-server
+go install -a ./btcplex-server ./cmd/...
 export GOBIN=$OLD_GOBIN
 export OLD_GOBIN=
